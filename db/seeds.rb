18fd6cb7d69308a7102c2493849572fd29cb53b6
tetsumi <kaiko-16t@keio.jp>
2020-08-31 04:44:24 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..100).each do |number|
+  Task.create(content: 'タスク ' + number.to_s, status: 'ステータス ' + number.to_s)
+end
